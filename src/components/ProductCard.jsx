ca648621e14381be1da3e020877e9e0e8263d348
vero <jaramillomoralesvero@gmail.com>
2026-03-26 01:32:40 -0500

    feat: mostrar rating con estrellas dinamicas

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,6 +1,10 @@
 import "../styles/ProductCard.css";
 
 export const ProductCard = ({ product }) => {
+  const fullStars = Math.floor(product.rating);
+  const hasHalfStar = product.rating % 1 !== 0;
+  const emptyStars = 5 - fullStars - (hasHalfStar ? 1 : 0);
+
   const discountedPrice = (
     product.price - (product.price * product.discountPercentage) / 100
   ).toFixed(2); 
@@ -17,7 +21,11 @@ export const ProductCard = ({ product }) => {
 
       <p className="product-final">Precio final: ${discountedPrice}</p>
 
-      <p className="product-rating">Rating: ⭐ {product.rating}</p>
+      <div className="product-rating">
+        {"★".repeat(fullStars)}
+        {hasHalfStar && "⯨"}
+        {"☆".repeat(emptyStars)}
+      </div>
     </div>
   );
 };
